3e5eea90d2cb32907c9723e8c584a1fc3552dfaa
AzureAD\KrystinGolden <krystin.golden@actionstep.com>
2025-07-04 10:23:18 -0400

    Test4

--- src/Controllers/ProviderController.cs
+++ src/Controllers/ProviderController.cs
@@ -17,7 +17,7 @@ public class ProductsController : Controller
     [Route("/products")]
     public IActionResult GetAll()
     {
-        var test = "test3";
+        var test = "test4";
         return new JsonResult(_repository.GetProducts());
     }
 
